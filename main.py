8fde30607a2badb12bff92445c573fd1c130cb06
Mahammad Mahmudov <78232377+MahammadMahmudov23@users.noreply.github.com>
2024-09-16 23:42:02 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+# Python program to translate speech to text and text to speech
+
+import speech_recognition as sr  # Make sure 'SpeechRecognition' is imported as 'speech_recognition'
+import pyttsx3
+import PyAudio
+# Initialize the recognizer
+r = sr.Recognizer()
+
+# Function to convert text to speech
+def SpeakText(command):
+    # Initialize the engine
+    engine = pyttsx3.init()
+    engine.say(command)
+    engine.runAndWait()
+
+# Loop infinitely for user to speak
+while True:
+    # Exception handling to handle exceptions at runtime
+    try:
+        # Use the microphone as source for input.
+        with sr.Microphone() as source2:
+            # Wait for a second to let the recognizer adjust the energy threshold
+            # based on the surrounding noise level
+            r.adjust_for_ambient_noise(source2, duration=0.2)
+
+            # Listen for the user's input
+            audio2 = r.listen(source2)
+
+            # Using Google to recognize audio
+            MyText = r.recognize_google(audio2)
+            MyText = MyText.lower()
+
+            print(f"Did you say: {MyText}")
+            SpeakText(MyText)
+
+    except sr.RequestError as e:
+        print(f"Could not request results; {e}")
+
+    except sr.UnknownValueError:
+        print("Unknown error occurred")
